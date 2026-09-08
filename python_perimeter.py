5aee664ee1a95e2269b254625af235b9af6984a3
tejucyber <sankabattulatejasri@gmail.com>
2026-09-08 13:25:28 +0530

    perimeter of the rectangle

---
+++ python_perimeter.py
@@ -0,0 +1,6 @@
+length=float(input("enter the length"))
+breadth=float(input("enter the breadth"))
+area=length*breadth
+perimeter=2*length+breadth
+print("AREA=",area)
+print("PERIMETER=",perimeter)
