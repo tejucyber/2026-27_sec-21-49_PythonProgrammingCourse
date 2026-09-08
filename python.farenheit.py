6a1a6c4f2f7217fa1ce5b1b7e2ed9c5948f941c0
tejucyber <sankabattulatejasri@gmail.com>
2026-09-08 13:26:19 +0530

    celsius to farenheit

---
+++ python.farenheit.py
@@ -0,0 +1,4 @@
+#Aim:write a python program to convert temperature from celsius to fahrenheit.
+celsius=int(input("ENTER THE TEMPERATURE IN Celsius:"))
+fahrenheit=(9/5*celsius)+32
+print("TEMPERATURE IN Fahrenheit=",fahrenheit)
