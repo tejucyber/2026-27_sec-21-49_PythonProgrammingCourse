084214a8e483a5ede4dfbef6a1fa5b37c42686f8
tejucyber <sankabattulatejasri@gmail.com>
2026-09-08 13:27:19 +0530

    swap the values

---
+++ swap_program.py
@@ -0,0 +1,7 @@
+a=int(input("Enter a number a: "))
+b=int(input("Enter a number b: "))
+a=a+b
+b=a-b   
+a=a-b
+print("After swapping the values : a =", a)
+print("After swapping the values : b =", b)
