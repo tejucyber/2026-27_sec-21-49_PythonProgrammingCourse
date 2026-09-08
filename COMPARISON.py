a5b4865de8686e46b0b262a60d916fefdeabd43e
tejucyber <sankabattulatejasri@gmail.com>
2026-09-08 13:29:18 +0530

    comparison

---
+++ COMPARISON.py
@@ -0,0 +1,25 @@
+a = 4
+b = 8
+result= a > b #greater than comparison operator
+print("result of ",a, ">",b, "is : ", result )
+
+result= a < b #less than comparison operator
+print("result of" ,a, "<" ,b, "is : ", result)
+
+result= a == b #equal to comparison operator
+print("result of " ,a, "==" ,b, "is : ", result)
+
+result= a!=b
+print("result of " ,a, "!=" ,b, "is : ", result)
+
+result= a < b
+print("result of " ,a, "<" ,b, "is : ", result)
+
+ result= a > b
+print("result of " ,a, ">" ,b, "is : ", result)
+
+result= a <= b
+print("result of " ,a, "<=" ,b, "is : ", result)
+
+result= a >= b
+print("result of " ,a, ">=" ,b, "is : ", result)
